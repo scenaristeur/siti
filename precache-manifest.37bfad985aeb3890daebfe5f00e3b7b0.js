1f407f5c12072c559c0c8c7bf48c558a60490d2d
Scenaristeur <scenaristeur@gmail.com>
2021-06-10 01:42:54 +0200

    p5

---
+++ precache-manifest.37bfad985aeb3890daebfe5f00e3b7b0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a647fb3b758aa54470d",
+    "url": "/siti/css/app.06582b92.css"
+  },
+  {
+    "revision": "23c1e4cbf3eb494e42c0",
+    "url": "/siti/css/chunk-4583010f.e02c5362.css"
+  },
+  {
+    "revision": "eed3a851ff42e5243a4d",
+    "url": "/siti/css/chunk-633c44b1.52d67b05.css"
+  },
+  {
+    "revision": "0e9ed90c425b41fbb475",
+    "url": "/siti/css/chunk-vendors.66e8e58e.css"
+  },
+  {
+    "revision": "a1c6c67312e70be5b5227a44a1136955",
+    "url": "/siti/index.html"
+  },
+  {
+    "revision": "40c2882f36abd8f56297",
+    "url": "/siti/js/about.bc657de1.js"
+  },
+  {
+    "revision": "7a647fb3b758aa54470d",
+    "url": "/siti/js/app.d98baa41.js"
+  },
+  {
+    "revision": "e9f1a78295d7c6ffeebb",
+    "url": "/siti/js/chunk-2848b080.66c07fc7.js"
+  },
+  {
+    "revision": "0607c2e86157ba57981c",
+    "url": "/siti/js/chunk-2a879daf.40339ba9.js"
+  },
+  {
+    "revision": "52357da4ce304b73f942",
+    "url": "/siti/js/chunk-2d0b6733.f5043a66.js"
+  },
+  {
+    "revision": "d64396a65e6f4e4a19c8",
+    "url": "/siti/js/chunk-2d0bceed.61e0d04c.js"
+  },
+  {
+    "revision": "01d4c6ea135900066b45",
+    "url": "/siti/js/chunk-2d0df304.20e01808.js"
+  },
+  {
+    "revision": "4c94c8d2bdbe5bede7ba",
+    "url": "/siti/js/chunk-2d217b05.b90c0727.js"
+  },
+  {
+    "revision": "90607a8cc5106b82e629",
+    "url": "/siti/js/chunk-2d22c8c2.d1cae828.js"
+  },
+  {
+    "revision": "23c1e4cbf3eb494e42c0",
+    "url": "/siti/js/chunk-4583010f.4d55935e.js"
+  },
+  {
+    "revision": "eed3a851ff42e5243a4d",
+    "url": "/siti/js/chunk-633c44b1.09265996.js"
+  },
+  {
+    "revision": "0e9ed90c425b41fbb475",
+    "url": "/siti/js/chunk-vendors.083ce356.js"
+  },
+  {
+    "revision": "e8e1cfaf867233aa0b1156f304e5ddb3",
+    "url": "/siti/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/siti/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/siti/precache-manifest.91033a1b10eeac570947d68e2d47a593.js"
+  "/siti/precache-manifest.37bfad985aeb3890daebfe5f00e3b7b0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "siti"});
